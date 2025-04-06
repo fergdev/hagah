bd22dd8e6fbe86842ceb5bb354484ee32f9862aa
fergdev <fergus.a.hewson@outlook.com>
2025-04-07 08:09:53 +1200

    Fix for detekt.

--- buildSrc/src/main/kotlin/LocalProperties.kt
+++ buildSrc/src/main/kotlin/LocalProperties.kt
@@ -3,7 +3,6 @@
 import java.util.Properties
 
 fun Properties.openApiKey() = this["openApiKey"] as? String
-fun Properties.storeFilePath() = "certificates/hagah.jks"
 fun Properties.storePassword() = this["storePassword"] as String
 fun Properties.keyPassword() = this["keyPassword"] as String
 fun Properties.keyAlias() = this["keyAlias"] as String
