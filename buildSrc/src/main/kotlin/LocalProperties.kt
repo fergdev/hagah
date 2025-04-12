a14cdc86b06a0a044f65ecdd75cdfca9a3abd4c5
fergdev <fergus.a.hewson@outlook.com>
2025-04-13 07:10:23 +1200

    update for ios workflow.

--- buildSrc/src/main/kotlin/LocalProperties.kt
+++ buildSrc/src/main/kotlin/LocalProperties.kt
@@ -7,4 +7,4 @@ fun Properties.storePassword() = this["storePassword"] as String
 fun Properties.keyPassword() = this["keyPassword"] as String
 fun Properties.keyAlias() = this["keyAlias"] as String
 fun Properties.mockData() = (this["mockData"] ?: "") as String
-fun Properties.keyStorePath() = this["keyStorePath"] as String
+fun Properties.keyStorePath() = this["keyStorePath"] ?: ""
